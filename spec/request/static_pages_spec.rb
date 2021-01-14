4fa24f08b5d454e416d520dcf47746fb41b0432b
Ubuntu <ubuntu@ip-172-31-40-179.us-east-2.compute.internal>
2021-01-14 12:46:59 +0000

    rspec-insert-rails2

--- spec/request/static_pages_spec.rb
+++ spec/request/static_pages_spec.rb
@@ -1,9 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
+  
+  
+  
+  describe 'root' do
+    it "response successfully" do
+      get root_path
+      expect(response).to be_successful
+    end
+    it 'returns a 200 response' do
+      get root_path
+      expect(response).to have_http_status (200)
+    end
+    
+  end
+
   describe 'home' do
+    it "response successfully" do
+      get static_pages_home_path
+      expect(response).to be_successful
+    end
     it 'returns a 200 response' do
       get static_pages_home_path
+      expect(response).to have_http_status (200)
+    end
+    
+  end
+  
+  describe 'about' do
+    it "response successfully" do
+      get static_pages_about_path
+      expect(response).to be_successful
+    end
+    it 'returns a 200 response' do
+      get static_pages_about_path
+      expect(response).to have_http_status (200)
+    end
+    
+  end
+  
+  describe 'help' do
+    it "response successfully" do
+      get static_pages_help_path
+      expect(response).to be_successful
+    end
+    it 'returns a 200 response' do
+      get static_pages_help_path
       expect(response).to have_http_status (200)
     end
     
