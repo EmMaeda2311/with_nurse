a111f90d6f848de95a41db7c02f885ef48b857c4
Ubuntu <ubuntu@ip-172-31-40-179.us-east-2.compute.internal>
2021-01-14 13:33:11 +0000

    application.html.erbの追加

--- spec/request/static_pages_spec.rb
+++ spec/request/static_pages_spec.rb
@@ -1,26 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-  
+  let(:base_title) { 'With Nurse ナース専用のソーシャルメディア' }
   
   
   describe 'root' do
-    it "response successfully" do
-      get root_path
-      expect(response).to be_successful
-    end
     it 'returns a 200 response' do
       get root_path
       expect(response).to have_http_status (200)
+      expect(response.body).to match(/<title>#{base_title}<\/title>/i)
     end
     
   end
 
   describe 'home' do
-    it "response successfully" do
-      get static_pages_home_path
-      expect(response).to be_successful
-    end
     it 'returns a 200 response' do
       get static_pages_home_path
       expect(response).to have_http_status (200)
@@ -29,10 +22,6 @@ RSpec.describe "StaticPages", type: :request do
   end
   
   describe 'about' do
-    it "response successfully" do
-      get static_pages_about_path
-      expect(response).to be_successful
-    end
     it 'returns a 200 response' do
       get static_pages_about_path
       expect(response).to have_http_status (200)
@@ -41,10 +30,6 @@ RSpec.describe "StaticPages", type: :request do
   end
   
   describe 'help' do
-    it "response successfully" do
-      get static_pages_help_path
-      expect(response).to be_successful
-    end
     it 'returns a 200 response' do
       get static_pages_help_path
       expect(response).to have_http_status (200)
