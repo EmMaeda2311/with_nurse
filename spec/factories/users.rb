ae64747de919138c6a31df1a2d2d9d4c73fd69ab
Ubuntu <ubuntu@ip-172-31-40-179.us-east-2.compute.internal>
2021-02-01 02:31:49 +0000

    logout test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :tester , class: User do
-        sequence(:id, 1)
+        # sequence(:id, 1)
         username { "tester" }
         sequence(:email,"tester1@example.com") 
         # { |n| "#{n}tester@example.com" }
@@ -8,5 +8,13 @@ FactoryBot.define do
         accepted {true}
   end
   
+  
+  factory :another_teser , class: User do
+        username{ "teser2" }
+        sequence(:email,"tester1@example.com") 
+        # { |n| "#{n}tester@example.com" }
+        password { "foobar" }
+        accepted {true}
+  end
   
 end
--- spec/request/sessions_spec.rb
+++ spec/request/sessions_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 RSpec.describe "Sessions" ,type: :request do
   let(:tester) { FactoryBot.create(:tester)}
   let(:tester_params) { attributes_for(:tester) }
-  let(:guest_user_params) { attributes_for(:user, name: "") }  
+  let(:guest_user_params) { attributes_for(:user, name: "") }
+  let(:another_teser){FactoryBot.create(:another_teser)}
   
   describe "get new page" do  
     it "sign_in returns a 200 request" do
-      get sign_in_path
+      get new_user_session_path
       expect(response).to have_http_status (200)
     end
   end
@@ -16,7 +17,12 @@ RSpec.describe "Sessions" ,type: :request do
     before do
       sign_in tester
     end
-
+    
+    # it "is signed_in" do
+    #   expect(user_signed_in?).to be_truthy
+    # end
+    
+    
     it "edit_page responds a 200" do
       get edit_user_registration_path
       expect(response).to have_http_status (200)
@@ -28,13 +34,42 @@ RSpec.describe "Sessions" ,type: :request do
       expect(response).to have_http_status (200)
     end
     
+    
+    
+    it "log_out_test" do 
+      sign_out tester
+      get edit_user_registration_path
+      expect(response).to have_http_status (302)
+    end
   end
   
   
-  # describe "as a guest" do 
-  #   it "redirects to the login page" do
-  #     get users_show_path(tester)
-  #     expect(response).to redirect_to sign_in_path
-  #   end
-  # end
+  describe "as a guest" do 
+    it "edit_page responds a 302" do
+      get edit_user_registration_path
+      expect(response).to have_http_status (302)
+    end
+    
+     it "show_page responds a 302" do
+      user_id = tester.id
+      get "/users/#{user_id}"
+      # expect(response).to have_http_status (302)
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+  
+  
+  describe "another_user not access mypage" do
+    before do
+      sign_in another_teser
+    end
+    
+    
+    #他のユーザーの編集ページにアクセスできないテスト→できちゃう
+    # it "dont access tester edit page " do
+    #   get edit_user_registration_path(tester)
+    #   expect(response).to have_http_status (302)
+    # end
+    
+  end
 end
