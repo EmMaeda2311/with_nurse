47ff19ab2c7b8ef4625c5da8538e1f11d6d7fa1a
EmMaeda <nurseride0723@gmail.com>
2021-02-16 23:07:14 +0900

    comfirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable,
          :lockable, :omniauthable, omniauth_providers: [:google_oauth2]
-        # :confirmable,
+        
   
   before_save {email.downcase!}
   validates :username, presence: true, length: { maximum: 50}
