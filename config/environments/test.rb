fb17de78387c634478e8b9241141c3d1f8a67a12
EmMaeda <nurseride0723@gmail.com>
2021-03-08 21:06:57 +0900

    imges upload setting

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,6 +6,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+  config.active_storage.service = :local
+  
   config.cache_classes = false
   config.action_view.cache_template_loading = true
 
