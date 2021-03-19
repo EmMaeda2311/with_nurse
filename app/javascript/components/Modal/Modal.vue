8e90a041aa3e686cea1b59e96da0c58975c3ec49
EmMaeda <nurseride0723@gmail.com>
2021-03-20 00:08:37 +0900

    modal3

--- app/javascript/components/Modal/Modal.vue
+++ app/javascript/components/Modal/Modal.vue
@@ -1,24 +1,61 @@
 <template>
-  <div>
-   
+  <div class="my-photo">
+    <div v-if="uploadedImage == '' ">
+      <div v-if="image == nil ">
+        <img src="../../../images/nurse.png" alt="">
+      </div>
+      <div v-else>
+        <img 
+        v-show="image"
+        :src="image" 
+        alt="">
+      </div>
+    </div>
+    <div v-else>
+      <img
+        v-show="uploadedImage"
+        class="preview-item-file"
+        :src="uploadedImage"
+        alt=""
+      />
+    </div>
+
+
     <div class="el-modal" :aria-hidden="isOpen ? 'false' : 'true'">
+
       <div class="el-modal__holder">
-        <button @click="close()"><font-awesome-icon :icon= "['fas', 'times']" /></button>
-        <div class="change-avatar">サムネイルを変更する</div>
+        <div class="x-icon" @click="close()"><font-awesome-icon :icon= "['fas', 'times']" /></div>
+        
+        <label for="user-avatar" class="change-avatar">サムネイルを変更する
+        <input @change="onFileChange" id="user-avatar" type="file" accept="image/png, image/jpeg" style= "display: none;">
+        </label>
+        
         <div class="delete-avatar">初期に戻す</div>
       </div>
+
       <div @click="close()" class="el-modal__overlay"></div>
     </div>
-    <div @click="open()"><font-awesome-icon :icon= "['fas', 'portrait']" /></div>
+    
+    <div @click="open()" class="portrait"><font-awesome-icon :icon= "['fas', 'portrait']" />
+    </div>
   </div>
+
 </template>
 
 <script>
+import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+// CSRFトークンの取得とリクエストヘッダへの設定をしてくれます
+axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken()
+
 export default {
+  props:'userAvatar',
   name: "Modal",
   data: function() {
     return {
-      isOpen: false
+      image = this.userAvatar,
+      isOpen: false,
+      uploadedImage: '',
     };
   },
   methods: {
@@ -27,6 +64,19 @@ export default {
     },
     close: function() {
       this.isOpen = false;
+    },
+    onFileChange(e) {
+    const files = e.target.files || e.dataTransfer.files;
+    this.createImage(files[0]);
+    },
+
+    createImage(file){
+      const reader = new FileReader();
+      reader.onload = e => {
+        this.uploadedImage = e.target.result;  
+      };
+      reader.readAsDataURL(file);
+
     }
   }
 };
