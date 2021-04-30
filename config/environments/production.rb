624fe1771ced3ff0550545ee80bafe2664d31ff7
EmMaeda <nurseride0723@gmail.com>
2021-04-30 22:33:05 +0900

    mail test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,8 +117,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
+    domain: 'gmail.com',
     port: 587,
-    user_name: ENV['LOCAL_EMAIL_ADDRESS'],
+    user_name: ENV['EMAIL_ADDRESS'],
     password: ENV['EMAIL_PASSWORD'],
     authentication: :plain,
     openssl_verify_mode: 'none',
